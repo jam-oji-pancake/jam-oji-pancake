c13e18ff768be3bf3bde0464e83dfe10eac68910
EC2 Default User <ec2-user@ip-172-31-5-246.ec2.internal>
2021-07-16 10:48:49 +0000

    create Item model

--- naganocake/db/schema.rb
+++ naganocake/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_041509) do
+ActiveRecord::Schema.define(version: 2021_07_16_102414) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "image_id"
+    t.text "introduction"
+    t.integer "price"
+    t.boolean "is_sale", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

2f4da35fbbd2aa5b7c90f08d791e8ea158735aa5
JINJIN1103 <bonousou@gmail.com>
2021-07-17 04:58:05 +0000

    リトライ

--- naganocake/app/models/cart.rb
+++ naganocake/app/models/cart.rb
@@ -1,4 +1,4 @@
 class Cart < ApplicationRecord
   belongs_to :custmer
-  belongs_to :
+  belongs_to :item
 end
--- naganocake/app/models/custmer.rb
+++ naganocake/app/models/custmer.rb
@@ -3,9 +3,9 @@ class Custmer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :deliveries, dependent: :destroy
   has_many :carts, dependent: :destroy
   has_many :orders, dependent: :destroy
-  
+
 end

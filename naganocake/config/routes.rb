0eb75e4677ed5e159a5238da01dcaf4833040522
EC2 Default User <ec2-user@ip-172-31-12-98.ec2.internal>
2021-07-17 02:55:38 +0000

    controller/public/orders/customers/deliveries

--- naganocake/config/routes.rb
+++ naganocake/config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'deliveries/index'
+    get 'deliveries/edit'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/caution'
+  end
+  namespace :public do
+    get 'orders/index'
+    get 'orders/show'
+    get 'orders/select'
+    get 'orders/confirm'
+    get 'orders/finish'
+  end
   devise_for :custmers
   devise_for :admins
   devise_for :users

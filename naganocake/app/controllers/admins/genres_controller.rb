4afb33b158d3c4814eccebf56e409ac0c3b6d710
EC2 Default User <ec2-user@ip-172-31-8-251.ec2.internal>
2021-07-17 02:55:46 +0000

    [Create]AdminController-oders.items.genres

---
+++ naganocake/app/controllers/admins/genres_controller.rb
@@ -0,0 +1,15 @@
+class Admins::GenresController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
--- naganocake/config/routes.rb
+++ naganocake/config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  namespace :admins do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admins do
+    get 'items/index'
+    get 'items/show'
+    get 'items/new'
+    get 'items/edit'
+  end
+  namespace :admins do
+    get 'orders/index'
+    get 'orders/show'
+  end
   devise_for :custmers
   devise_for :admins
   devise_for :users

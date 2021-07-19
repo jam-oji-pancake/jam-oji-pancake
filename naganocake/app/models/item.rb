2e7b1bd436d4f665852b3d14a83e6b2dad6e8bc8
EC2 Default User <ec2-user@ip-172-31-5-246.ec2.internal>
2021-07-19 04:27:43 +0000

    change is_sale status

--- naganocake/app/models/item.rb
+++ naganocake/app/models/item.rb
@@ -2,9 +2,11 @@ class Item < ApplicationRecord
    # belongs_to :genre
    has_many :carts, dependent: :destroy
    has_many :order_items, dependent: :destroy
-   
+
    attachment :image
-   
-   
-   
+
+   enum is_sale: {販売中: true, 販売停止中: false }
+
+
+
 end

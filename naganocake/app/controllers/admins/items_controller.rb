d08ffce01540ed72adc6ec4ca76514fd2ca7587e
EC2 Default User <ec2-user@ip-172-31-5-246.ec2.internal>
2021-07-20 10:33:15 +0000

    change branch

--- naganocake/app/controllers/admins/items_controller.rb
+++ naganocake/app/controllers/admins/items_controller.rb
@@ -1,7 +1,6 @@
 class Admins::ItemsController < ApplicationController
 
   def index
-    @items = Item.page(params[:page]).per(10)
   end
 
   def show
@@ -14,21 +13,26 @@ class Admins::ItemsController < ApplicationController
 
   def create
     item = Item.new(item_params)
-    item.save(item_params)
+    item.save
     redirect_to admins_item_path(item)
     # redirect_to request.referer
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+     item = Item.find(params[:id])
+     item.update(item_params)
+     redirect_to  admins_item_path(item)
   end
 
   
+
 private
   def item_params
-    params.require(:item).permit(:image, :name, :introduction, :price, :is_sale )
+    params.require(:item).permit(:image, :name, :introduction, :price, :is_sale,:genre_id )
   end
 
 end

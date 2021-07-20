f1321cfb62a90874f05fee70ce4efc19b3ff8727
EC2 Default User <ec2-user@ip-172-31-8-251.ec2.internal>
2021-07-20 00:59:26 +0000

    [Add]EditItem

--- naganocake/db/schema.rb
+++ naganocake/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_113956) do
+ActiveRecord::Schema.define(version: 2021_07_19_141147) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 2021_07_18_113956) do
     t.boolean "is_sale", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "genre_id"
   end
 
   create_table "order_items", force: :cascade do |t|

5a0f58c66e34490cdbe0d145b4186ede3597cbeb
JINJIN1103 <bonousou@gmail.com>
2021-07-23 02:13:17 +0000

    2

--- naganocake/app/controllers/public/carts_controller.rb
+++ naganocake/app/controllers/public/carts_controller.rb
@@ -1,9 +1,14 @@
 class Public::CartsController < ApplicationController
   
+  before_action :set_line_item, only: [:add_item, :destroy]
+  before_action :set_user
+  before_action :set_cart
+  
   def my_cart
   end
   
   def add_item
+    @cart =Cart.new
   end
   
   def update_item
@@ -13,6 +18,19 @@ class Public::CartsController < ApplicationController
   end
   
   def destroy_all
+  end
+  
+private
+  def set_user
+    @user = current_user
+  end
+
+  def set_line_item
+    @line_item = current_cart.line_items.find_by(product_id: params[:product_id])
+  end
+
+  def set_cart
+    @cart = current_cart
   end
   
 end

56610953a25c0173be23f1fc65246453ede15db2
EC2 Default User <ec2-user@ip-172-31-8-251.ec2.internal>
2021-07-23 07:17:06 +0000

    [Update]Orderselect[fix]アソシエーション

--- naganocake/app/controllers/public/deliveries_controller.rb
+++ naganocake/app/controllers/public/deliveries_controller.rb
@@ -1,38 +1,39 @@
 class Public::DeliveriesController < ApplicationController
-  
+
   def index
+  @customer = current_customer
   @delivery = Delivery.new
-  @deliveries = Delivery.all
+  @deliveries = @customer.deliveries
   end
-  
+
   def create
   delivery = Delivery.new(delivery_params)
   delivery.customer_id = current_customer.id
   delivery.save
   redirect_to  deliveries_path
   end
-  
+
   def edit
   @delivery = Delivery.find(params[:id])
   end
-   
+
   def update
   delivery = Delivery.find(params[:id])
   delivery.update(delivery_params)
   redirect_to  deliveries_path
   end
-  
+
   def destroy
   delivery = Delivery.find(params[:id])
-  delivery.destroy
+  delivery.destroy(delivery_params)
   redirect_to  deliveries_path
   end
-  
+
   private
-  
+
   def delivery_params
-   params.require(:delivery).permit(:post_code, :address, :name)
+   params.require(:delivery).permit(:post_code, :address, :name, :customer_id)
   end
-  
+
 end
 
--- naganocake/app/models/customer.rb
+++ naganocake/app/models/customer.rb
@@ -3,11 +3,18 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :orders
+  has_many :deliveries
+  has_many :carts
 
   # enum is_valid: {有効: true, 退会: false }
 
   def active_for_authentication?
     super && (self.is_valid == false)
+  end
+
+  def full_name
+    self.first_name + " " + self.last_name
   end
 
 end

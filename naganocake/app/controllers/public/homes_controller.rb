1fb9021976fb37c7c5436021eb0e1ef2fd10e590
EC2 Default User <ec2-user@ip-172-31-8-251.ec2.internal>
2021-07-24 02:14:09 +0000

    orderselect、confirm編集

--- naganocake/app/controllers/public/homes_controller.rb
+++ naganocake/app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
-    @all_items = Item.all
-    @items = Item.page(params[:page]).per(4).reverse_order
+    @all_items =  Item.where(is_sale: true)
+    @items = @all_items.page(params[:page]).per(4).reverse_order
     @genres = Genre.all
   end
 

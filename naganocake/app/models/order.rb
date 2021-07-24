cf3837f2a6c8e4c3259bb79c6e8809241696ba49
EC2 Default User <ec2-user@ip-172-31-8-251.ec2.internal>
2021-07-24 08:30:55 +0000

    [Create]OrderFnish.create

--- naganocake/app/models/order.rb
+++ naganocake/app/models/order.rb
@@ -3,7 +3,12 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
 
   enum payment: {クレジットカード: 1, 銀行振込: 0 }
-  enum delivery_adress: {ご自身の住所: 0, 新しいお届け先: 2 }
+  enum delivery_adress: {ご自身の住所: 0, 登録済み住所: 1, 新しいお届け先: 2 }
+  enum order_status: {入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4 }
+
+  def total_count
+    self.order_items.sum(:count)
+  end
 
   # def perfect_price
   #   self.send_cost + Cart.subtotal_price
